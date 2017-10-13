f053f0303ac300311217b2bc18c03b69c6037fa3
mirabilos <t.glaser@tarent.de>
2017-10-13 15:58:52 +0200

    remove the HpwDb demo class, it was abysmal as an example

--- www/index.php
+++ www/index.php
@@ -1,20 +1,32 @@
 <?php
 
-/* this must come first */
+/* this must come first (and adds minijson implicitly) */
 require_once('util.php');
-/* include db.php here or, indirectly, via autoloader (see below) */
+/* now this where necessary */
+require_once('db.php');
 
-$db = new HpwDb();
+$logId = db_insert_one('id',
+    'INSERT INTO log (ts, ip) VALUES (now(), $1)',
+    array($_SERVER["REMOTE_ADDR"]));
 
 html_header(array('title' => 'Hello PHP World v' . HPW_VERSION));
 
-if (!($schemavsn = HpwDb::GetSchemaVersion())) {
+$res = db_query_params('SELECT version FROM z_schema_version', array());
+$schemavsn = false;
+if ($res) {
+	if (db_numrows($res) !== 1)
+		util_debugJ(true, 'Could not retrieve DB schema version: ' .
+		    db_numrows($res) . ' entries found');
+	else
+		$schemavsn = db_result($res, 0, 'version');
+}
+if ($schemavsn === false) {
 	echo '<p>Could not access the database.</p>' . "\n";
 } else {
 	echo '<table border="1">';
 	echo '<tr><th>ID</th><th>Blabla</th></tr>' . "\n";
-	$db->Query('SELECT * FROM content');
-	while (($row = $db->NextRow())) {
+	$res = db_query_params('SELECT * FROM content', array());
+	while (($row = db_fetch_array($res))) {
 		echo '<tr><th>' . $row['id'] . '</th><td>' .
 		    util_html_encode($row['blabla']) . '</td></tr>' . "\n";
 	}
@@ -22,9 +34,9 @@ if (!($schemavsn = HpwDb::GetSchemaVersion())) {
 
 	printf("<p>DB-Schema Version %d</p>\n", $schemavsn);
 
-	printf("<p>I logged your visit as ID #%d</p>\n", $db->getLogId());
-	$db->Query('SELECT * FROM log WHERE id=$1', array($db->getLogId()));
-	if (($row = $db->NextRow())) {
+	printf("<p>I logged your visit as ID #%d</p>\n", $logId);
+	$res = db_query_params('SELECT * FROM log WHERE id=$1', array($logId));
+	if (($row = db_fetch_array($res))) {
 		printf("<p>#%d - %s - %s</p>\n", $row['id'], $row['ts'],
 		    util_html_encode($row['ip']));
 	} else {

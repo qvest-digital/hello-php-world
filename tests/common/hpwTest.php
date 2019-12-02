4dfd87fdc4d90356c8f4c9fbf382788360e1568d
mirabilos <t.glaser@tarent.de>
2019-12-03 00:16:23 +0100

    add some very basic loading tests for the rest

---
+++ tests/common/hpwTest.php
@@ -0,0 +1,36 @@
+<?php
+/*-
+ * Small test for hello-php-world’s demonstration class
+ *
+ * Copyright © 2019
+ *	mirabilos <t.glaser@tarent.de>
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+require_once(dirname(__FILE__) . '/../phpFUnit.php');
+
+class HPW_Tests extends PHPUnit_Framework_TestCase {
+	public function testHPW() {
+		$s = error_reporting(-1);
+		require_once(dirname(__FILE__) . '/../../common/hpw.php');
+
+		/* currently just a syntax and load test */
+		$this->assertTrue(true);
+
+		error_reporting($s);
+	}
+}
